614c6f25f8fb60860df9f1883e9d6bf8a79d25ae
斉藤　直之 <saitounaoyuki@ns.local>
2017-04-12 02:03:27 +0900

    Fix register

--- index.js
+++ index.js
@@ -43,18 +43,24 @@ app.get('/register', function(req, res) {
 app.post('/register', function(req, res) {
   var userName = req.body.user_name;
   var email = req.body.email;
+  // console.log(req.body.email);/
   var password = req.body.password;
   var emailExistsQuery = 'SELECT * FROM users WHERE email = "' + email + '" LIMIT 1';
-  console.log('ok')
-  console.log(emailExistsQuery)
   var registerQuery = 'INSERT INTO users (user_name, email, password) VALUES ("' + userName + '", ' + '"' + email + '", ' + '"' + password + '")';
+
   connection.query(emailExistsQuery, function(err, email) {
-    console.log(email);
+
     var emailExists = email.length === 1;
+    console.log(emailExists);
     if (emailExists) {
       res.render('register', {
         title: '新規会員登録',
-        emailExists: '既に登録されているメールアドレスです'
+        emailExists: '既に登録されているメールアドレスです',
+        activeMainMenu: {
+          about: '',
+          contact: '',
+          product: ''
+        },
       });
     } else {
       connection.query(registerQuery, function(err, rows) {
