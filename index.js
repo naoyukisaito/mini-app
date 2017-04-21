29944e7b72e0b3eff6a23284bd8da5f91706f03c
naoyuki_saito <naoyuki_saito@waku-2.com>
2017-04-21 19:04:53 +0900

    Fixing session

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var session = require('express-session');
 var connection = require('./mysqlConnection');
 // console.log(connection);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/assets', express.static(__dirname + '/public'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
 app.post('/contact', function(req, res) {
   // console.log(req.body);
   res.redirect('/');
@@ -19,12 +27,37 @@ app.set('view engine', 'pug');
 app.get('/', function(req, res) {
   // console.log(req.params);
   // console.log(res.params);
+  if (req.session.user_id) {
+    console.log(req.session.user_id);
+  } else {
+    console.log('session failed');
+  }
   res.render('index', {
     title: 'Mini App',
     activeMainMenu: {
       about: '',
       contact: ''
     },
+  });
+});
+
+app.post('/', function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var query = 'SELECT user_id FROM users WHERE email = "' + email + '" AND password = "' + password + '" LIMIT 1';
+  connection.query(query, function(err, rows) {
+    var userId = rows.length? rows[0].user_id: false;
+    console.log(userId);
+    console.log(req.session);
+    if (userId) {
+      req.session.user_id = userId;
+      res.redirect('/');
+    } else {
+      res.render('/index', {
+        title: 'ログイン',
+        noUser: 'メールアドレスとパスワードが一致するユーザーはいません'
+      });
+    }
   });
 });
 
@@ -53,7 +86,7 @@ app.post('/register', function(req, res) {
     var emailExists = email.length === 1;
     // console.log(emailExists);
     if (emailExists) {
-      console.log(emailExists);
+      // console.log('exist!!');
       res.render('register', {
         title: '新規会員登録',
         emailExists: '既に登録されているメールアドレスです',
@@ -109,7 +142,7 @@ app.get('/product', function(req, res) {
 
 app.get('/order', function(req, res) {
   connection.query('SELECT * FROM product_detail', function(err, rows) {
-    console.log(rows.id);
+    // console.log(rows.id);
   // console.log('req.query = ', req.query);
     // var product = rows.find(function(row) {
     //     console.log(row.length);
@@ -117,7 +150,7 @@ app.get('/order', function(req, res) {
     //   return row.id === req.query.id;
     // });
     var product = rows[req.query.id - 1];
-    console.log(product);
+    // console.log(product);
 
     res.render('order', {
       title: 'Mini App - product / order',
@@ -146,8 +179,8 @@ app.get('/thanks', function(req, res) {
 // app.get('/')
 
 app.post('/order/:id', function(req, res) {
-  console.log(req);
-  console.log('ls');
+  // console.log(req);
+  // console.log('ls');
   var query = 'INSERT INTO user_info (name, email) VALUES ("' + req.body.name + '", ' + '"' + req.body.email + '")';
   connection.query(query, function(err, rows) {
     res.redirect('/thanks');
